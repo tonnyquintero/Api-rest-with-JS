76d452fe5a54faa3fada8396c1272fed720a7db0
tonny187 <tonyoanthony@gmail.com>
2022-11-03 06:03:01 -0500

    creating addFavourites function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?';
 const API_KEY = 'api_key=live_fAkiaYHjeH5pgHCvyr3FKA79HR5kj5ZoRL1BJU7waZYFkdqfoaKqcb8aNZYzrH9A';
 
 const spanError = document.getElementById('error');
@@ -16,11 +16,16 @@ async function reloadRandomMichis() {
 
     } else  {
         const img1 = document.getElementById('img1');
+        const btn1 = document.getElementById('btn1')
         const img2 = document.getElementById('img2');
+        const btn2 = document.getElementById('btn2')
 
 
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavouritesMichi(data[0].id)
+        btn2.onclick = () => saveFavouritesMichi(data[1].id)
 
     }
 }
@@ -34,18 +39,33 @@ async function loadFavoritesmMichis() {
 
     if (res.status !== 200) {
         spanError.innerHTML = 'Hubo un error: ' + res.status + data.message;
+    } else {
+        data.forEach(kitty => {
+            const section = document.getElementById('favoritesMichis')
+            const article = document.createElement('article')
+            const img = document.createElement('img')
+            const btn = document.createElement('button')
+            const btnText = document.createTextNode('Sacar al gatito de favoritos');
+
+            img.src = kitty.image.url
+            img.width = 150
+            btn.appendChild(btnText);
+            article.appendChild(img)
+            article.appendChild(btn)
+            section.appendChild(article)
+        });
     }
 
 }
 
-async function saveFavouritesMichis() {
+async function saveFavouritesMichi(id) {
     const resi = await fetch(`${API_URL_FAVORITES}&${API_KEY}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            image_id: '2u0'
+            image_id: id
         }),
 });
 
@@ -60,6 +80,6 @@ async function saveFavouritesMichis() {
 }
 
 
-saveFavouritesMichis()
+saveFavouritesMichi()
 reloadRandomMichis();
 loadFavoritesmMichis()
