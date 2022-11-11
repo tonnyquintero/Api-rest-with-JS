5e896686f8be29a7ba6ff23ff80441e498cd5b3d
tonny187 <tonyoanthony@gmail.com>
2022-11-11 18:29:50 -0500

    adding the style.css file

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const  api = axios.create({
 
 api.defaults.headers.common['X-API-KEY'] = 'live_fAkiaYHjeH5pgHCvyr3FKA79HR5kj5ZoRL1BJU7waZYFkdqfoaKqcb8aNZYzrH9A';
 
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=6';
 const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=';
 const API_KEY = 'live_fAkiaYHjeH5pgHCvyr3FKA79HR5kj5ZoRL1BJU7waZYFkdqfoaKqcb8aNZYzrH9A';
 const API_DELETE_FAVOURITE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=`
@@ -26,14 +26,36 @@ async function reloadRandomMichis() {
         const img1 = document.getElementById('img1');
         const btn1 = document.getElementById('btn1')
         const img2 = document.getElementById('img2');
-        const btn2 = document.getElementById('btn2')
+        const btn2 = document.getElementById('btn2');
+        const img3 = document.getElementById('img3');
+        const btn3 = document.getElementById('btn3');
+        const img4 = document.getElementById('img4');
+        const btn4 = document.getElementById('btn4');
+        const img5 = document.getElementById('img5');
+        const btn5 = document.getElementById('btn5');
+        const img6 = document.getElementById('img6');
+        const btn6 = document.getElementById('btn6');
 
 
         img1.src = data[0].url;
         img2.src = data[1].url;
+        img3.src = data[2].url;
+        img4.src = data[3].url;
+        img5.src = data[4].url;
+        img6.src = data[5].url;
+        img1.width = 150
+        img2.width = 150
+        img3.width = 150
+        img4.width = 150
+        img5.width = 150
+        img6.width = 150
 
         btn1.onclick = () => saveFavouritesMichi(data[0].id)
         btn2.onclick = () => saveFavouritesMichi(data[1].id)
+        btn3.onclick = () => saveFavouritesMichi(data[2].id)
+        btn4.onclick = () => saveFavouritesMichi(data[3].id)
+        btn5.onclick = () => saveFavouritesMichi(data[4].id)
+        btn6.onclick = () => saveFavouritesMichi(data[5].id)
 
     }
 }
@@ -55,10 +77,7 @@ async function loadFavoritesmMichis() {
     } else {
         const section = document.getElementById('favoritesMichis')
         section.innerHTML = ""
-        const h2 = document.createElement('h2')
-        const h2Text = document.createTextNode('Gatitos Favoritos')
-        h2.appendChild(h2Text)
-        section.appendChild(h2)
+        
 
         data.forEach(kitty => {
             const article = document.createElement('article')
@@ -68,6 +87,7 @@ async function loadFavoritesmMichis() {
 
             img.src = kitty.image.url
             img.width = 150
+            img.height = 150
             btn.appendChild(btnText);
             btn.onclick = () => deleteFavouritesMichi(kitty.id)
             article.appendChild(img)
